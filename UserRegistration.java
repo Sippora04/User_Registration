3802cd9516247e8102caacb1f427623fe7854c5b
Sippora04 <sippora.toppo95@gmail.com>
2020-10-27 16:00:21 +0530

    Added: First Name Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,11 +1,31 @@
 package com.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+	
+	//UC1: checking first name valid or not
+	public static void validateName (String firstName) {
+		String NamePattern = "^[A-Z]{1}[a-zA-z]{2,}";
+		Pattern pattern = Pattern.compile(NamePattern);
+		Matcher matcher = pattern.matcher(firstName);		
+		if(matcher.find())
+			System.out.println(firstName + " is valid");			
+        else 
+        	{
+        	System.out.println("First name starts with Capital letter and has minimum 3 charachters.");
+        	}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to User Registration Problem");
-
+		Scanner sc = new Scanner(System.in);
+		String firstName = "";
+		System.out.println("Enter Firstname: ");
+		firstName = sc.nextLine();
+		validateName (firstName);
+		sc.close();
 	}
 
 }
